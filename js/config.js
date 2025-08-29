da3d4019d2eec500896aac6e55773bebf3fd8a4b
Pikitucho <richiesouza35@gmail.com>
2025-08-28 19:16:09 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDqCT_iOBToHDR7sRQnH_mUmwN5V_RXj58",
+    authDomain: "codex-vitae-app.firebaseapp.com",
+    projectId: "codex-vitae-app",
+    storageBucket: "codex-vitae-app.firebasestorage.app",
+    messagingSenderId: "1078038224886",
+    appId: "1:1078038224886:web:19a322f88fc529307371d7",
+    measurementId: "G-DVGVB274T3"
+};
--- js/main.js
+++ js/main.js
@@ -1,13 +1,4 @@
 // --- Firebase Initialization ---
-const firebaseConfig = {
-    apiKey: "AIzaSyDqCT_iOBToHDR7sRQnH_mUmwN5V_RXj58",
-    authDomain: "codex-vitae-app.firebaseapp.com",
-    projectId: "codex-vitae-app",
-    storageBucket: "codex-vitae-app.firebasestorage.app",
-    messagingSenderId: "1078038224886",
-    appId: "1:1078038224886:web:19a322f88fc529307371d7",
-    measurementId: "G-DVGVB274T3"
-};
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
 const db = firebase.firestore();
