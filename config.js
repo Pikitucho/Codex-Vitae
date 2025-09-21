44ee8b09c13c3c7dccd24070c3d6a0255e069790
Pikitucho <richiesouza35@gmail.com>
2025-09-21 08:58:00 -0400

    Update Firebase API key

---
+++ config.js
@@ -0,0 +1,11 @@
+window.__CODEX_CONFIG__ = {
+  firebaseConfig: {
+    apiKey: "AIzaSyDqCT_iOBToHDR7sRQnH_mUmwN5V_RXj58",
+    authDomain: "codex-vitae-app.firebaseapp.com",
+    projectId: "codex-vitae-app",
+    storageBucket: "codex-vitae-app.appspot.com",
+    messagingSenderId: "107802832688486",
+    appId: "1:107802832688486:web:3d4c2f40568f6fa4569ad5"
+  },
+  backendUrl: "http://localhost:8080"
+};
