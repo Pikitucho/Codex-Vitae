6d1ab501391b842d6ac53f6c59a787eb7c4f1e70
Pikitucho <richiesouza35@gmail.com>
2025-09-26 14:20:24 -0400

    Add color grading pass and debug controls

--- js/pipeline/webgl-pipeline.js
+++ js/pipeline/webgl-pipeline.js
@@ -6,13 +6,41 @@ import { RGBELoader } from 'https://unpkg.com/three@0.160/examples/jsm/loaders/R
 
 window.CVPipeline = Object.assign(window.CVPipeline || {}, {
   EffectComposer, RenderPass, UnrealBloomPass, ShaderPass, RGBELoader,
-  createComposer(renderer, scene, camera, { bloom = true, grade = false } = {}) {
+  createComposer(renderer, scene, camera, { bloom = true, grade = true } = {}) {
     const composer = new EffectComposer(renderer);
-    composer.addPass(new RenderPass(scene, camera));
+    const renderPass = new RenderPass(scene, camera);
+    composer.addPass(renderPass);
+
     if (bloom) {
       const pass = new UnrealBloomPass(undefined, 0.9, 0.4, 0.8);
       composer.addPass(pass);
+      composer.__bloom = pass;
     }
+
+    if (grade && window.CVGradeShader) {
+      const gradePass = new ShaderPass(window.CVGradeShader);
+      const originalSetSize = composer.setSize.bind(composer);
+      composer.setSize = function setSize(w, h) {
+        originalSetSize(w, h);
+        if (gradePass.uniforms?.uResolution?.value?.set) {
+          gradePass.uniforms.uResolution.value.set(w, h);
+        }
+      };
+      if (gradePass.uniforms?.uResolution?.value) {
+        if (typeof renderer.getSize === 'function') {
+          renderer.getSize(gradePass.uniforms.uResolution.value);
+        } else if (renderer.domElement) {
+          gradePass.uniforms.uResolution.value.set(
+            renderer.domElement.width || renderer.domElement.clientWidth || 1,
+            renderer.domElement.height || renderer.domElement.clientHeight || 1
+          );
+        }
+      }
+      composer.addPass(gradePass);
+      composer.__grade = gradePass;
+    }
+
+    composer.__renderPass = renderPass;
     return composer;
   }
 });
